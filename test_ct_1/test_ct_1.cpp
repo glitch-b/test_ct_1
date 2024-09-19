f33d51e3280a6aeb37f9077012cc67d204ea9618
Student <Student@PC-SE5113-13>
2024-09-19 13:29:37 +0200

    Add project files.

---
+++ test_ct_1/test_ct_1.cpp
@@ -0,0 +1,15 @@
+﻿// test_ct_1.cpp : Defines the entry point for the application.
+//
+
+#include "test_ct_1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	system("pause");
+
+	return 0;
+}
